57c656699f89175642ef8d2f5cfca0550622c524
ksergey <bladzher@yandex.ru>
2015-06-26 13:42:49 +0300

    Add server and client .c

---
+++ client/client.c
@@ -0,0 +1,108 @@
+/**
+* @author: Sergey Kudryavtsev <bladzher@yandex.ru>
+*  HELP:
+* ...........PC name: /ksergey/
+* ...........Ultrabook name: /bladzher/
+*******************************************************************************
+ */
+#define pathToLoad "Загрузки/"
+#define sizeName 256
+#define pathToList "list.xml"
+#define BUF_SIZE 1024
+
+char message[1024];
+int sock;                // дескриптор сокета
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include "functions/fileTransferRecv.h"
+#include "functions/fileTransferSend.h"
+#include "functions/displayListFiles.h"
+#include "functions/deleteFile.h"
+#include "functions/downloadFile.h"
+#include "functions/addFile.h"
+#include "functions/navigation.h"
+
+void settingsClient(){
+
+    /*     Указываем параметры сервера    */
+    struct sockaddr_in addr; // структура с адресом
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET; // домены Internet
+    addr.sin_port = htons(3425); // Порт
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");    //addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    /*END*/
+
+    int check;
+    if ((check = inet_pton(addr.sin_family, "127.0.0.1", &addr.sin_addr)) < 0) {
+        perror("Error: первый параметр не относится к категории корректных адресов.");
+    } else if (!check) {
+        perror("Error: второй параметр не содержит корректного IP-адреса.");
+    }
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {// создание TCP-сокета
+        perror("socket");
+        exit(1);
+    }
+
+    if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) { // установка соединения с сервером
+        perror("connet");
+        close(sock);
+        exit(2);
+    } else {
+        printf("Вы успешно подключились к системе передачи файлов!\n");
+    }
+
+}
+
+void workingClient(){
+
+    int counter = 0, number = 0, *getTeam = &number;
+    while (1) {
+
+        if (counter == 0) {
+            recv(sock, message, sizeof(message), 0);
+            counter++;
+        }
+
+        printf("%s\n", message);
+
+        do {
+            printf("Введите команду серверу[0-7]:");
+            if((scanf("%d", &number)) == 0){
+                printf("\n Ошибока ввода :) Как-нибудь в другой раз ...\nЖду 2 секунды и закругляюсь...\n");
+                number = 4;
+                sleep(2);
+            }
+
+        } while (number > 7 || number < 0);
+
+        printf("Отправляю сообщение серверу.\n");
+
+        if ((send(sock, (char *) &number, sizeof(int), 0)) < 0) {
+            perror("send[0]");
+        }
+
+        printf("%d\n", *getTeam);
+
+        if (navigation(*getTeam) == 1) {
+            remove(pathToList);
+            break;
+        }
+    }
+    close(sock);
+}
+
+int main() {
+
+    settingsClient();
+    workingClient();
+
+    return 0;
+}
---
+++ server/server.c
@@ -0,0 +1,185 @@
+/**
+* @author: Sergey Kudryavtsev <bladzher@yandex.ru>
+*  HELP:
+* ...........PC name: /ksergey/
+* ...........Ultrabook name: /bladzher/
+*******************************************************************************
+ */
+#define textMenu "\n========================\nМЕНЮ:\n0)Отобразить список файлов.\n1)Добавить файл.\n2)Удалить файл.\n3)Скачать файл.\n4)Выход.\n5)Выключить сервер.\n6)Обновить список файлов.\n7)Очистить экран\n========================\n"
+#define pathToFolder "files"
+#define pathToFolers "files/"
+#define pathToList "list.xml"
+#define sizeName 256
+#define BUF_SIZE 1024
+
+int numberOfFiles = 0;
+int sock, listener;      // дескрипторы сокетов
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include "functions/loadList.h"
+#include "functions/fileTransferRecv.h"
+#include <signal.h>
+#include <stdlib.h>
+#include "functions/fileTransferSend.h"
+#include "functions/addFile.h"
+#include "functions/deleteFile.h"
+#include "functions/downloadFile.h"
+#include "functions/navigation.h"
+
+//void die(int sig)
+//{
+//    puts("\nПрощай, жестокий мир...\n");
+//    exit(1);
+//}
+//
+// if(catch_signal(SIGINT, ) == )
+//
+//int catch_signal(int sig, void (*handler)(int))
+//{
+//    struct sigaction action;
+//    action.sa_handler = handler;
+//    sigemptyset(&action.sa_mask);
+//    action.sa_flags = 0;
+//    return sigaction(sig, &action, NULL);
+//}
+
+void runDaemon(){
+    pid_t pidDaemonFTS = daemon(0,0); //     демон - запуск отдельно от терминала...
+    if ( pidDaemonFTS == -1 ) {
+        //close(ls);
+        perror( "demonize error!\n");
+    }
+}
+
+void settingsServer(){
+
+    if ((listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {// создаем сокет для входных подключений
+        perror("socket");
+        exit(1);
+    }
+
+    /*     Указываем параметры сервера    */
+    struct sockaddr_in addr; // структура с адресом
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");    //addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    /*    END block    */
+
+    int check, yes = 1;
+    if ((check = inet_pton(addr.sin_family, "127.0.0.1", &addr.sin_addr)) < 0) {
+        perror("Error: первый параметр не относится к категории корректных адресов.");
+    } else if (!check) {
+        perror("Error: второй параметр не содержит корректного IP-адреса.");
+    }
+
+    setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes);
+
+    if (bind(listener, (struct sockaddr *) &addr, sizeof(struct sockaddr_in)) < 0) { // связываемся с сетевым устройство. Сейчас это устройство lo - "петля", которое используется для отладки сетевых приложений
+        perror("bind");
+        close(listener);
+        exit(2);
+    }
+
+    if ((listen(listener, 1)) == -1) { // очередь входных подключений
+        perror("listen");
+        close(listener);
+        exit(0);
+    }
+
+    printf("Сервер запущен!\n\nОжидание подключения:\n");
+}
+
+void workingServer(){
+
+    int counter = 0, number = 0, result = 0, *getCommand= &number;
+    pid_t pid;
+
+    while (result != 1) { //для подкл.клиентов.
+
+        if ((sock = accept(listener, NULL, NULL)) < 0) {// принимаем входные подключение и создаем отделный сокет для каждого нового подключившегося клиента
+            perror("accept");
+            close(listener);
+            exit(3);
+        }
+
+        pid = fork();
+        switch (pid) {
+            case -1:
+                perror("fork");
+                exit(1);
+            case 0:
+                close(listener);
+
+                printf("К серверу подключился клиент!\n");
+
+                while (1) {
+                    if (counter == 0) {
+                        send(sock, textMenu, sizeof(textMenu), 0);
+                        counter++;
+                    }
+                    do {
+                        printf("Ожидаем команду:\n");
+                        if ((recv(sock, (void *) &number, sizeof(int), 0)) < 0) {
+                            perror("recv[0]");
+                        }
+                        if(*getCommand < 7 && *getCommand >= 0){
+                            break;
+                        }else{
+                            printf("Получена неверная команда!\n");
+                        }
+                    }while(1);
+                    printf("Получена команда от клиента: %d\n", *getCommand);
+
+                    result = navigation(*getCommand);
+                    if (result == 1) {
+                        close(sock);
+                        break;
+                    }else if(result == 2){
+                        //close(sock);
+                        break;
+                    }
+                }
+            default:
+                //wait(0);
+                close(sock);
+//                close(listener);
+                //_exit(0);
+        }
+    }
+}
+
+int main(int argc, char * argv[]) {
+
+    if(argc == 2 && strcmp(argv[1], "daemon") == 0){
+        printf("%s", argv[1]);
+        runDaemon();
+    }
+
+    settingsServer();
+    workingServer();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
